0ba54ba944bc5eec0fc7556ad5ed0266b773217b
satvik sinha <sinhasatvik214@gmail.com>
2024-09-17 21:02:58 +0530

    Main file

---
+++ bandnamegenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+usr_city=input("Enter the name of the city you grew up in\n")
+usr_pet=input("Enter the name of your pet\n")
+print("Your band name could be:" + usr_city+ " " +usr_pet )
